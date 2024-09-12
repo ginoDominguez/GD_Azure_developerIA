d77dd975b930cb69001b506dad5e5cd47521c70c
gino dominguez <gino_2do@hotmail.com>
2024-09-12 14:01:16 +0200

    Proyecto Notas

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -98,6 +98,67 @@ int fahrenheit= 94;
 Console.WriteLine( "The temperature is: " +  Math.Round( (fahrenheit-32) * ((decimal)5/9),1)  + " Celsius" );
 
 
+//// project Overview: students and qualifications.
+///
+
+
+
+decimal Sofia_1, Sofia_2,Sofia_3,Sofia_4,Sofia_5;
+decimal Nicolas_1, Nicolas_2,Nicolas_3,Nicolas_4,Nicolas_5;
+decimal Zahirah_1, Zahirah_2,Zahirah_3,Zahirah_4,Zahirah_5;
+decimal Jeong_1, Jeong_2,Jeong_3,Jeong_4,Jeong_5;
+
+decimal Sofia_prom, Nicolas_prom, Zahirah_prom, Jeong_prom;
+
+Sofia_1=93m;
+Sofia_2=87m;
+Sofia_3=98m;
+Sofia_4=95m;
+Sofia_5=100m;
+Nicolas_1=80m;
+Nicolas_2=83m;
+Nicolas_3=82m;
+Nicolas_4=88m;
+Nicolas_5=85m;
+Zahirah_1=84m;
+Zahirah_2=96m;
+Zahirah_3=73m;
+Zahirah_4=85m;
+Zahirah_5=79m;
+Jeong_1=90m;
+Jeong_2=92m;
+Jeong_3=98m;
+Jeong_4=100m;
+Jeong_5=97m;
+
+//int[ ] numeros = {93, 87, 98, 95,100};
+
+
+Sofia_prom= (Sofia_1+Sofia_2 + Sofia_3+ Sofia_4 + Sofia_5)/5;
+Nicolas_prom= (Nicolas_1+ Nicolas_2+Nicolas_3+Nicolas_4+Nicolas_5)/5;
+Zahirah_prom=(Zahirah_1+ Zahirah_2+Zahirah_3+Zahirah_4+Zahirah_5)/5;
+Jeong_prom= (Jeong_1+ Jeong_2+Jeong_3+Jeong_4+Jeong_5);
+
+
+
+Console.WriteLine("La nota de Sohia es: " + Sofia_prom);
+Console.WriteLine("La nota de Nicolas es: " + Nicolas_prom);
+Console.WriteLine("La nota de Zahirah es: " + Zahirah_prom);
+Console.WriteLine("La nota de Jeong es: " + Jeong_prom);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
