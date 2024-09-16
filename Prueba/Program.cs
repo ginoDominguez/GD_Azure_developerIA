da641d75198ce1cae1fbc1c0ac506f20ebc14664
gino dominguez <gino_2do@hotmail.com>
2024-09-16 11:12:18 +0200

    Dias Para expirar la suscripcion

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -1,57 +1,60 @@
 ﻿//Random numbers and get the max number:
 using System.Numerics;
 
-Random dice = new Random();
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+Random random = new Random();
 
-int total =0;
+int discountPercentage = 0;
+var continua = true;
 
-List<int> list = new List<int>();
-//list.Add(dice.Next(1,7));
+// Your code goes here
 
-
-for( int i=1; i<=3;i++)
+while (continua)
 {
-   list.Add( dice.Next(1,7));
-   total= total + list[i-1];
-   Console.WriteLine(list[i-1]);
-    
-};
-
-Console.WriteLine("El valor total es: "+ total);
-
-//Console.WriteLine($"Dice roll:  + {list[0]} + {list[1]} + {list[2]}  = {total} ");
-//Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+    int daysUntilExpiration = random.Next(12);
+    Console.Clear();
+    Console.WriteLine("Total de dias hasat que expire segun random:" + daysUntilExpiration);
 
 
-if( ( list[0] == list[1])  ||  ( list[1] == list[2]) || ( list[0] == list[2])   )
-{
-    Console.WriteLine(" Ypu rolled doubles! += 2 bonus");
-    total+=2;
+    if (daysUntilExpiration == 0)
+    {
+        
+        Console.WriteLine("Your subscription has expired.");
+
+
+    }
+    else
+    {
+
+        if (daysUntilExpiration <= 1)
+        {
+            Console.WriteLine("Your subscription expires within a day! Renew now and save 20%!");
+
+
+        }
+        else
+        {
+            if (daysUntilExpiration <= 5)
+            {
+                Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days. Renew now and save 10%!");
+            }
+            else
+            {
+
+                if (daysUntilExpiration <= 10)
+                {
+                    Console.WriteLine("Your subscription will expire soon. Renew now!");
+                };
+
+            }
+
+        }
+
+    }
+    var tecla = Console.ReadKey();
+    if (tecla.KeyChar == 'x')
+    { continua = false; }
+
+
 
 }
-
-
-if( ( list[0] == list[1])  &&  ( list[1] == list[2])    )
-{
-    Console.WriteLine(" Ypu rolled doubles! += 6 bonus");
-    total+=6;
-
-}
-
-
-if(total > 14)
-{
-
-    Console.WriteLine("You winr");
-}
-else{
-    Console.WriteLine("Sorry you lose");
-}
-
-/// Identificar valores duplicados:
-/// 
-
