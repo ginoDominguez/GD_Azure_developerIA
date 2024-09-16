c6d47c646a12596de886cf2a342c5ad029dda7f6
gino dominguez <gino_2do@hotmail.com>
2024-09-16 13:39:10 +0200

    BusquedaPalabraalInicio

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -11,3 +11,30 @@ foreach(var item in farudulentOrderIDs)
 {
     Console.WriteLine(item);
 }
+
+
+//string[] datos = new string['B123','C234','A345','C15','B177','G3003', 'C235', 'B179'];
+
+///string[] datos = new string[8];
+
+string[] datos = {"B123","C234","A345","C15","B177","G3003", "C235", "B179"};
+
+
+
+foreach(var item in datos)
+{
+    if (item.StartsWith("B"))
+{
+    Console.WriteLine("The name starts with 'B'!");
+}
+
+
+}
+
+
+
+
+
+
+
+
