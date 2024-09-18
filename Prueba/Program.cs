43f07522e223846e94cae2faff0a6d1e7856dde7
gino dominguez <gino_2do@hotmail.com>
2024-09-18 13:25:20 +0200

    Compare Do-while and while

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Role playing game battle challenge
-
+/*
 int lifeHeroe=10;
 int lifeMonster=10;
 int attack=0;
@@ -21,9 +21,6 @@ do
          Console.WriteLine("Monster attack:"+attack + " Heroe remaining life:" + lifeHeroe);
 
     }
-
-
-
 }
 while(lifeHeroe>0 && lifeMonster>0);
 
@@ -38,6 +35,79 @@ else
     Console.WriteLine("Monster wins");
 }
 Console.ReadKey();
+
+*/
+
+//string? readResult;
+/*
+string readResult=null;
+Console.WriteLine("Enter a string:");
+do
+{
+    readResult = Console.ReadLine();
+} while (readResult == null);
+*/
+
+string? readResult;
+bool validEntry = false;
+Console.WriteLine("Enter a string containing at least three characters:");
+do
+{
+    readResult = Console.ReadLine();
+    if (readResult != null)
+    {
+        if (readResult.Length >= 3)
+        {
+            validEntry = true;
+        }
+        else
+        {
+            Console.WriteLine("Your input is invalid, please try again.");
+        }
+    }
+} while (validEntry == false);
+
+/*
+// capture user input in a string variable named readResult
+int numericValue = 0;
+bool validNumber = false;
+
+validNumber = int.TryParse(readResult, out numericValue);
+*/
+
+string? readResult_1;
+bool validNumber = false;
+int numeric_value = 0;
+
+
+
+
+do
+{
+    Console.WriteLine("Ingrese un valor de 5 a 10");
+    readResult_1 = Console.ReadLine();
+
+    validNumber = int.TryParse(readResult_1, out numeric_value);
+
+    if (validNumber == true)
+    {
+        if( numeric_value >=5 && numeric_value<=10)
+        {
+            Console.WriteLine("El numero ingresado es de : " + numeric_value + "");
+
+
+        }
+        else
+        {
+            Console.WriteLine("Enter a number from 5 to 10");
+            validNumber=false;
+        }
+    }
+
+
+}
+while (validNumber==false);
+
 
 
 
