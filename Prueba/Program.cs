503ad71ee4eaea0812ca2c564f3892010cfb55c5
gino dominguez <gino_2do@hotmail.com>
2024-09-18 11:54:31 +0200

    DoWhile Example

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -1,21 +1,20 @@
-﻿
+﻿Random random = new Random();
+int current = random.Next(1, 11);
 
-for (int i = 0; i <= 100; i++)
+do
 {
-    if (i % 3 == 0 && i % 5 == 0)
-    {
-        Console.WriteLine($"{i}\t FizzBuzz");
-    }
-    else
-        {
-            if (i % 3 == 0)
-            { Console.WriteLine($"{i}\tFizz"); }
-            else if (i % 5 == 0)
-            {
-                Console.WriteLine($"{i}\tBuzz");
+    current = random.Next(1, 11);
 
-            }
-            else {Console.WriteLine($"{i}");}
-        }
-   
+    if (current >= 8) continue;
+
+    Console.WriteLine(current);
+} while (current != 7);
+
+/*
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
 }
+Console.WriteLine($"Last number: {current}");
+*/
