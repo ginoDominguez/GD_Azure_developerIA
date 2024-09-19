e677aa79a5e604de0f9a6a8bc668619744de8485
gino dominguez <gino_2do@hotmail.com>
2024-09-19 11:46:07 +0200

    combine string array values as strings and as integers

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -1,23 +1,38 @@
-﻿Console.WriteLine("Signed integral types:");
+﻿/*int first = 2;
+string second = "7";
+int result = first + int.Parse(second);
+Console.WriteLine(result);
+*/
 
-Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
-Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
-Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
-Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
-Console.WriteLine($"double   : {double.MinValue} to {double.MaxValue}");
+/*string value1 = "5";
+string value2 = "7";
+int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+Console.WriteLine(result);
+*/
 
-
-Console.WriteLine("");
-Console.WriteLine("Unsigned integral types:");
-
-Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
-Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
-Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
-Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+//combine string array values as strings and as integers
 
 
-Console.WriteLine("");
-Console.WriteLine("Floating point types:");
-Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
-Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
-Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+using System.Linq.Expressions;
+using Microsoft.VisualBasic;
+
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+double resultado=0.0;
+string cadena = ""; 
+
+for (int i = 0; i<=(values.Length-1); i++)
+{
+    if(double.TryParse((values[i].ToString()), out resultado))
+    {
+        resultado+= resultado ;
+
+    }
+    else{
+        cadena= cadena + values[i];
+
+    }
+}
+
+Console.WriteLine("Message= " + cadena);
+Console.WriteLine("Total: " + resultado);
+
