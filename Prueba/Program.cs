976da80ce4ad1e705f57025fbfd88e00906e25ef
gino dominguez <gino_2do@hotmail.com>
2024-09-19 13:38:31 +0200

    Using Array Reverse

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -1,61 +1,28 @@
-﻿/*int first = 2;
-string second = "7";
-int result = first + int.Parse(second);
-Console.WriteLine(result);
-*/
+﻿using System.Runtime.CompilerServices;
 
-/*string value1 = "5";
-string value2 = "7";
-int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
-Console.WriteLine(result);
-*/
+int[] pallets = { 1, 2, 3, 4 };
 
-//combine string array values as strings and as integers
+Console.WriteLine("Sorted...");
 
-/*
-using System.Linq.Expressions;
-using Microsoft.VisualBasic;
-
-string[] values = { "12.3", "45", "ABC", "11", "DEF" };
-double resultado=0.0;
-string cadena = ""; 
-
-for (int i = 0; i<=(values.Length-1); i++)
+Array.Sort(pallets);
+Array.Clear(pallets, 0, 2);
+foreach (var pallet in pallets)
 {
-    if(double.TryParse((values[i].ToString()), out resultado))
-    {
-        resultado+= resultado ;
-
-    }
-    else{
-        cadena= cadena + values[i];
-
-    }
+    Console.WriteLine($"-- {pallet}");
 }
 
-Console.WriteLine("Message= " + cadena);
-Console.WriteLine("Total: " + resultado);
-
-*/
 
 
-int value1 = 11;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
-decimal result1, result2;
-float result3;
+/////Discover Split() and Join()
+///
+string value = "abc123";
+string value3="";
+char[] valueArray = value.ToCharArray();
 
-// Your code here to set result1
-// Hint: You need to round the result to nearest integer (don't just truncate)
 
-result1=value1/value2;
+Array.Reverse(valueArray);
 
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {Math.Round(result1,0)}");
+value3 = new string(valueArray);
 
-// Your code here to set result2
-result2= value2/ (decimal)value3;
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+Console.WriteLine(value3);
 
-// Your code here to set result3
-result3= value3/value1;
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
