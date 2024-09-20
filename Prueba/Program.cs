862cf6ccaca85f90f21720f5607821c5ba9a958f
gino dominguez <gino_2do@hotmail.com>
2024-09-20 10:59:14 +0200

    formatos con sring

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -1,20 +1,43 @@
-﻿using System.Runtime.ConstrainedExecution;
+﻿/*
+string first = "Hello";
+string second = "World";
+Console.WriteLine("{1} {0}!", first, second);
+Console.WriteLine("{0} {0} {0}!", first, second);
+*/
 
-string pangram = "The quick brown fox jumps over the lazy dog";
-string[] resultado= pangram.Split(" ");
-string output=string.Empty;
-
-
-for(int i=0; i <= (resultado.Length-1); i++)
-{
-    /*
-   Array.Reverse(resultado);
-   output= new string(resultado[i]);
-    Console.WriteLine(resultado[i]);
-    */
-    output+= string.Join("", resultado[i].ToCharArray().Reverse()) +" ";
-}
-Console.WriteLine(output);
+/// cambiar el culture Info:
+/// 
 
 
-//https://learn.microsoft.com/en-us/training/modules/csharp-arrays-operations/5-challenge-1
+using System.Globalization;
+
+CultureInfo.CurrentCulture = new CultureInfo("en-US"); //en-US    es-EU
+
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N} units");
+
+
+decimal measurement2 = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement2:N4} units");
+
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P}");
+
+
+decimal price2 = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price2 - salePrice), price2);
+
+Console.WriteLine(yourDiscount);
+
+
+
+string input = "Pad this";
+ Console.WriteLine(input.PadLeft(12));
+  Console.WriteLine(input.PadLeft(12,'-'));
