8e5811a50adc8ced19f69c4f42c260f09018d23f
ginoDominguez <gino_2do@hotmail.com>
2024-09-27 08:45:00 -0500

    InvitatioOnlyV2

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -6,7 +6,7 @@ int count = 0;
 RSVP("Rebecca", 1, "none", true);
 RSVP("Nadia", 2, "Nuts", true);
 RSVP("Linh", 2, "none", false);
-RSVP("Tony", 1, "Jackfruit", true);
+RSVP("Tony", 1, "Jackfruit", false);
 RSVP("Noor", 4, "none", false);
 RSVP("Jonte", 2, "Stone fruit", false);
 ShowRSVPs();
