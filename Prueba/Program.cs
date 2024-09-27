cf7c3d93a4cc87686a2f2b512da124177e6542e7
ginoDominguez <gino_2do@hotmail.com>
2024-09-27 08:18:52 -0500

    StringsReferecneGlobal

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -1,22 +1,15 @@
-﻿int[] array = {1, 2, 3, 4, 5};
+﻿string status = "Healthy";
 
-PrintArray(array);
-Clear(array);
-PrintArray(array);
+Console.WriteLine($"Start: {status}");
+SetHealth( false);
+Console.WriteLine($"End: {status}");
 
-void PrintArray(int[] array) 
+status="Malo";
+Console.WriteLine($"End2: {status}");
+
+
+void SetHealth(bool isHealthy) 
 {
-    foreach (int a in array) 
-    {
-        Console.Write($"{a} ");
-    }
-    Console.WriteLine();
-}
-
-void Clear(int[] array) 
-{
-    for (int i = 0; i < array.Length; i++) 
-    {
-        array[i] = 0;
-    }
+    status = (isHealthy ? "Healthy" : "Unhealthy");
+    Console.WriteLine($"Middle: {status}");
 }
