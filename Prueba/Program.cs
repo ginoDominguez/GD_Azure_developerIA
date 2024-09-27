08ebf24cbb26b129dacfa493b6b090c1f689eae2
ginoDominguez <gino_2do@hotmail.com>
2024-09-27 08:05:32 -0500

    CircleInfo

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -1,10 +1,25 @@
-﻿
+﻿double pi = 3.14159;
 
-PrintCircleArea(12);
+PrintCircleInfo(12);
+PrintCircleInfo(24);
+
 
 void PrintCircleArea(int radius)
 {
-    double pi = 3.14159;
+ 
     double area = pi * (radius * radius);
     Console.WriteLine($"Area = {area}");
+}
+
+void PrintCircleCircumference(int radius)
+{
+    double circumference = 2 * pi * radius;
+    Console.WriteLine($"Circumference = {circumference}");
+}
+
+void PrintCircleInfo(int radius) 
+{
+    Console.WriteLine($"Circle with radius {radius}");
+    PrintCircleArea(radius);
+    PrintCircleCircumference(radius);
 }
