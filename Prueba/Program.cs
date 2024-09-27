5b1e2a263cb827efa003ff4b1a77038b75f4f1b8
ginoDominguez <gino_2do@hotmail.com>
2024-09-27 06:25:20 -0500

    ExMethodWithParamters

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -1,30 +1,31 @@
-﻿/*
-Está ayudando a desarrollar un juego de rol multijugador masivo. Cada jugador tiene una estadística de suerte que puede afectar a sus probabilidades de encontrar tesoros raros. Cada día, un jugador puede hablar con un adivino en el juego que revela si su estadística de suerte es alta, baja o neutral.
+﻿// Metodos con parámetros:
 
-Actualmente el juego tiene código para generar la suerte de un jugador, pero no es reutilizable. La tarea consiste en crear un método tellFortune al que se pueda llamar en cualquier momento y reemplazar la lógica existente por una llamada al método.
+int[] schedule = {800, 1200, 1600, 2000};
 
-En este desafío, se le proporciona un código de partida. Debe decidir cómo crear el método tellFortune y cómo llamarlo.
-
-*/
-
-Random random = new Random();
-int luck = random.Next(100);
-
-string[] text = {"You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to"};
-string[] good = {"look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!"};
-string[] bad = {"fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life."};
-string[] neutral = {"appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature."};
-
-TellFortune();
+DisplayAdjustedTimes(schedule, 6, -6);
 
 
-void TellFortune()
+void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT) 
 {
-  Console.WriteLine("A fortune teller whispers the following words:");
-  
-string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
-for (int i = 0; i < 4; i++) 
+  int diff = 0;
+if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
 {
-    Console.Write($"{text[i]} {fortune[i]} ");
+    Console.WriteLine("Invalid GMT");
 }
+else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0) 
+{
+    diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+} 
+else 
+{
+    diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
 }
+
+for (int i = 0; i < times.Length; i++) 
+{
+    int newTime = ((times[i] + diff)) % 2400;
+    Console.WriteLine($"{times[i]} -> {newTime}");
+}
+
+}
+
