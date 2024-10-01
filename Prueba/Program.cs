d633bdd1bf29cd4b47285795aee56608db84e6c9
gino dominguez <gino_2do@hotmail.com>
2024-10-01 11:31:57 +0200

    To find coins to give ChangeV1

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -1,88 +1,41 @@
-<<<<<<< HEAD
-﻿
-///https://learn.microsoft.com/en-us/training/modules/csharp-modify-content/
-///https://learn.microsoft.com/es-es/training/modules/create-c-sharp-methods-parameters/6-exercise-challenge-email-addresses
-///
+﻿//// https://learn.microsoft.com/es-es/training/modules/create-c-sharp-methods-return-values/6-exercise-create-methods-return-arrays
+/// Encontrar monedas para dar el camnbio
+/// 
 
-string[,] corporate = 
+
+int[] TwoCoins(int[] coins, int target)
 {
-    {"Robert", "Bavin"}, {"Simon", "Bright"},
-    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
-    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
-};
-
-string[,] external = 
-{
-    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
-    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
-};
-
-string externalDomain = "hayworth.com";
-
-for (int i = 0; i < corporate.GetLength(0); i++) 
-{
-    DisplayEmail(first: corporate[i,0], lastname: corporate[i,1]);
-
-}
-
-for (int i = 0; i < external.GetLength(0); i++) 
-{
-    DisplayEmail(first: external[i,0], lastname: external[i,1], domain: externalDomain);
-}
-
-
-
-void DisplayEmail(string first, string lastname, string domain="contoso.com")
-{
-    string email= first.Substring(0, 2) + lastname;
-    email = email.ToLower();
-    Console.WriteLine(email + domain);
-=======
-﻿string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
-string[] rsvps = new string[10];
-int count = 0;
->>>>>>> b061fb90a3e0c40e9b82190c2a360e90620b9c7a
-
-}
-
-RSVP("Rebecca", 1, "none", true);
-RSVP("Nadia", 2, "Nuts", true);
-RSVP("Linh", 2, "none", false);
-RSVP("Tony", 1, "Jackfruit", false);
-RSVP("Noor", 4, "none", false);
-RSVP("Jonte", 2, "Stone fruit", false);
-ShowRSVPs();
-
-
-void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
-//void RSVP(string name, int partySize, string allergies, bool inviteOnly)
-{
-  if (inviteOnly)
-  {
-    bool found = false;
-    foreach (string guest in guestList)
+    for (int curr = 0; curr < coins.Length; curr++)
     {
-      if (guest.Equals(name))
-      {
-        found = true;
-        break;
-      }
+        for (int next = curr + 1; next < coins.Length; next++)
+        {
+            if (coins[curr] + coins[next] == target)
+            {
+                return new int[] { curr, next };
+            }
+
+        }
     }
-    if (!found)
-    {
-      Console.WriteLine($"Sorry, {name} is not on the guest list");
-      return;
-    }
-  }
-  rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
-  count++;
+
+    return new int[0];
 }
 
-void ShowRSVPs()
+
+int target = 60;
+int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+int[] result = TwoCoins(coins, target);
+
+if (result.Length == 0)
 {
-  Console.WriteLine("\nTotal RSVPs:");
-  for (int i = 0; i < count; i++)
-  {
-    Console.WriteLine(rsvps[i]);
-  }
+    Console.WriteLine("No two coins make change");
 }
+else
+{
+    Console.WriteLine($"Change found at positions {result[0]} and {result[1]}");
+}
+
+
+
+
+
+
