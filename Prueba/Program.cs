255efab55cdc0602e349b23d462db56ee9ca32b0
gino dominguez <gino_2do@hotmail.com>
2024-10-02 11:44:20 +0200

    EjemploDepuracion

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -3,4 +3,16 @@
 /// https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-return-values/10-summary#completion
 /// https://learn.microsoft.com/en-us/training/modules/guided-project-visit-petting-zoo/
 
+using System;
 
+string[] students = new string[] {"Sophia", "Nicolas", "Zahirah", "Jeong"};
+
+int studentCount = students.Length;
+
+try
+{
+    Console.WriteLine("The final name is: " + students[studentCount-1]);
+    }
+catch {
+    Console.WriteLine("No se pudo");
+}
