30bfeb0078ce6ef84a721e63c9b2e2f160366a18
gino dominguez <gino_2do@hotmail.com>
2024-10-03 10:37:54 +0200

    DebugTest

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("What is your name?");
+        //var name = Console.ReadLine();
+        var calculadora = new Calculadora();
+     
+        Console.WriteLine(calculadora.Suma(2,2));
+         Console.WriteLine(calculadora.Resta(2,2));
+
+
+    }
+}
+
---
+++ ConsoleApp1/calculadora.cs
@@ -0,0 +1,12 @@
+namespace ConsoleApp1;
+public class Calculadora
+{
+    public int Suma(int numero1, int numero2)
+    {
+        return numero1 + numero2;
+    }
+      public int Resta(int numero1, int numero2)
+    {
+        return numero1 - numero2;
+    }
+}
